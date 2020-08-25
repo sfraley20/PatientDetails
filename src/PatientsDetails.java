f815b2f891ee3586324152c06b66cac6c81779a5
sfraley20 <s.fraley.05@gmail.com>
2020-08-24 21:27:02 -0400

    patients details

---
+++ src/PatientsDetails.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PatientsDetails {
+
+    public static void main(String[] args) {
+        Scanner keybd = new Scanner(System.in);
+
+        boolean hasInsurance;
+
+        boolean hasInsurance = false;
+
+        String patientName;
+
+
+        String tempature;
+
+        String numberOfVisits;
+
+        System.out.println("Patient Details: ");
+
+        System.out.println("Name: " );
+
+        patientName = keybd.nextLine();
+
+        System.out.println("Does the patient have insurance " + false);
+
+        System.out.println("Body temprature: ");
+
+        tempature = keybd.nextLine();
+
+
+    }
+
+}
+//I coudln't figure out how to make a boolean that said that the patient didnt have insurance
